21b39fd74fa400573286b3fdaacc7976e857e081
manaswitha2000 <56600071+manaswitha2000@users.noreply.github.com>
2023-03-21 10:56:48 +0530

    Add files via upload

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,26 +5,41 @@ import static org.junit.Assert.*;
 public class AppTest{
     @BeforeClass
     public static void before_test() {
-        System.out.println("starting addition test");
+        System.out.println("starting all test");
     }
 
     @Test
-    public void positive_and_positive() {
-        System.out.println("test 1) Adding +ve and +ve integer numbers");
-        assertEquals("addition of 3.5 and 1.5 is not working correctly",5, Calculator.add(3.5f, 1.5f),0.00001);
+    public void sqroot() {
+        System.out.println("test 1) Finding square root of numbers");
+        assertEquals("Square root of 100 is not working correctly",10, Calculator.sqrt(100f),0.00001);
+        assertEquals("Square root of 10 is not working correctly",3.16227, Calculator.sqrt(10f),0.00001);
+        assertEquals("Square root of 0 is not working correctly",0, Calculator.sqrt(0f),0.00001);
     }
 
     @Test
-    public void positive_and_negative() {
-        System.out.println("test 2) Adding +ve and -ve integer numbers");
-        assertEquals("addition of 6.5 and -1.5 is not working correctly",5, Calculator.add(6.5f, -1.5f),0.00001);
+    public void fact() {
+        System.out.println("test 2) Finding factorial numbers");
+        assertEquals("Factorial of 5 is not working correctly",120, Calculator.factorial(5f),0.00001);
+        assertEquals("Factorial of 3 is not working correctly",6, Calculator.factorial(3f),0.00001);
+        assertEquals("Factorial of 0 is not working correctly",1, Calculator.factorial(0f),0.00001);
     }
 
     @Test
-    public void negative_and_negative() {
-        System.out.println("test 3) Adding -ve and -ve integer numbers");
-        assertEquals("addition of -6.5 and -1.5 is not working correctly",-8, Calculator.add(-6.5f, -1.5f),0.00001);
+    public void nlog() {
+        System.out.println("test 2) Finding factorial numbers");
+        assertEquals("Natural log of 10 is not working correctly",2.30258512496, Calculator.log(10f),0.00001);
+        assertEquals("Natural log of 4 is not working correctly",1.38629436112, Calculator.log(4f),0.00001);
+        assertEquals("Natural log of 0 is not working correctly",0, Calculator.log(1f),0.00001);
     }
+
+    @Test
+    public void powerfunc() {
+        System.out.println("test 2) Finding factorial numbers");
+        assertEquals("10 to the power of 2 is not working correctly",100, Calculator.power(10f, 2f),0.00001);
+        assertEquals("10 to the power of 2 is not working correctly",32, Calculator.power(2f, 5f),0.00001);
+        assertEquals("10 to the power of 2 is not working correctly",1, Calculator.power(1f, 100f),0.00001);
+    }
+
 
 
 }
